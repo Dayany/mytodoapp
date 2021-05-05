0c61ea679566912c367ec364c1c85c6e7b480089
Dayan Yamin <yamindayan@gmail.com>
2021-05-05 18:38:22 -0400

    Task reactivity to being clicked

--- src/components/Task/TabTask.tsx
+++ src/components/Task/TabTask.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MDBTabs, MDBTabsItem, MDBTabsLink } from 'mdb-react-ui-kit';
 
 
 const TabTask: React.FC = () => {
+  const [fillActive, setFillActive] = useState('tab1');
+
+  const handleFillClick = (value: string) => {
+    if (value === fillActive) {
+      return;
+    }
+
+    setFillActive(value);
+  };
 
   return (
     <MDBTabs pills className='card-header-tabs'>
       <MDBTabsItem>
-        <MDBTabsLink  active>
+        <MDBTabsLink className={ fillActive === 'tab1' ? "bg-secondary" : "" } onClick={() => handleFillClick('tab1')} active={fillActive === 'tab1'}>
           To do
         </MDBTabsLink>
       </MDBTabsItem>
       <MDBTabsItem>
-        <MDBTabsLink >
+        <MDBTabsLink className={ fillActive === 'tab2' ? "bg-success" : "" } onClick={() => handleFillClick('tab2')} active={fillActive === 'tab2'}>
           Done 
         </MDBTabsLink>
       </MDBTabsItem>
