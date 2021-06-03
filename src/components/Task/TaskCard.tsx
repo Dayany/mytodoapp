eaea37ddec7eb5dd3a64579d472efd6580c8f975
Dayan Yamin <yamindayan@gmail.com>
2021-06-03 15:23:15 -0400

    Set tasks to done/back to to-do

--- src/components/Task/TaskCard.tsx
+++ src/components/Task/TaskCard.tsx
@@ -7,20 +7,36 @@ interface Props {
 
 const TaskCard: React.FC<Props> = ({ task }) => {
   const borderStyle =  task.isDone ? "success" : "secondary";
+  const buttonStyle =  !task.isDone ? "success" : "secondary";
   const dispatch = useDispatch();
 
-  const deleteTask = async (uuid: string) => {
+  const deleteTask = async () => {
     const requestOptions = {
       method: "DELETE",
       headers: { "Content-Type": "application/json "}
     }
     try{ 
-      await fetch(`${process.env.REACT_APP_API_SERVER}tasks/${uuid}`, requestOptions)
-      .then((response) => dispatch({type: "REMOVE_TASK", payload: uuid}))
+      await fetch(`${process.env.REACT_APP_API_SERVER}tasks/${task.uuid}`, requestOptions)
+      .then((response) => dispatch({type: "REMOVE_TASK", payload: task.uuid}))
     } catch(error) {
       throw new Error(error)
     }
   }
+  const setTaskDone = async () => {
+    task.isDone = !task.isDone;
+    const requestOptions = {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json " },
+      body: JSON.stringify(task)
+    }
+    try{
+      await fetch(`${process.env.REACT_APP_API_SERVER}tasks/${task.uuid}`, requestOptions)
+      .then((response) =>{ dispatch({type: "MODIFY_TASK", payload: task})})
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+  
   return(
     <React.Fragment key={task.uuid}>
       <MDBCard shadow='0' border={ borderStyle } background='white' className='mb-3' >
@@ -33,9 +49,9 @@ const TaskCard: React.FC<Props> = ({ task }) => {
           <MDBCardTitle>{task.title}</MDBCardTitle>
           <MDBCardText>{task.content}</MDBCardText>
           <MDBBtn color="primary"  href='#'>Expand Task</MDBBtn>
-          <MDBBtn color="danger"  onClick={() => deleteTask(task.uuid)}>DELETE</MDBBtn>
+          <MDBBtn color="danger"  onClick={() => deleteTask()}>DELETE</MDBBtn>
           <span>  </span>
-          <MDBBtn color="success"  href='#'>Done</MDBBtn>
+          <MDBBtn color={buttonStyle}  onClick={() => setTaskDone()}>{task.isDone ? "Re-do" : "Done"}</MDBBtn>
         </MDBCardBody>
         <MDBCardFooter  border={ borderStyle } >Created:  {task.createdAt} | By: {task.user} </MDBCardFooter>
       </MDBCard>
